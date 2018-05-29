3ea9e190bde9eeab638df682eb4d6b032599ccd7
hsandstromOM <hosea@obviouslee.com>
2018-05-29 12:47:36 -0400

    added login service and user service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ROUTING } from './app.routing';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { RootComponent } from './root/root.component';
 import { LoginComponent } from './login/login.component';
 
-import { ROUTING } from './app.routing';
-
 @NgModule({
   declarations: [
-    RootComponent,
+  	RootComponent,
     LoginComponent
   ],
   imports: [
     BrowserModule,
     ROUTING,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [RootComponent]
